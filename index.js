f7dd5220318dd18bb02c5379df5b90b76a177b90
krypton <kelxwii@gmail.com>
2022-02-07 02:21:20 -0500

    added new features and fix previous bugs

--- index.js
+++ index.js
@@ -1,60 +1,123 @@
 require("dotenv").config();
-const express = require('express');
-const openai = require('openai');
+const express = require("express");
+const openai = require("openai");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
+const callbackUrl = "http://127.0.0.1:3000/callback";
 
-const dataSchema = new mongoose.Schema({
-    name: String
+const codestateSchema = new mongoose.Schema({
+  codeV: String,
+  State: String,
 });
 
-const cred = mongoose.model("cred", dataSchema);
+const codestate = mongoose.model("cred", codestateSchema);
+
+const tokenSchema = new mongoose.Schema({
+  accessTkn: String,
+  refreshTkn: String,
+});
+
+const tkn = mongoose.model("token", tokenSchema);
 
 const twitter = require("twitter-api-v2").default;
 const twitterClient = new twitter({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-})
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
 app.get("/", (req, res) => {
-    res.send("twitter bot ib landing page")
-})
-
+  res.send("twitter bot ib landing page");
+});
 
 //auth
 app.get("/auth", (req, res) => {
-    const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
-        "/callback",
-        { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
-      );
+  const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
+    callbackUrl,
+    { scope: ["tweet.read", "tweet.write", "users.read", "offline.access"] }
+  );
 
-    const newData = new cred({
-        name: "codeVerifier and state",
-        codeVerifier,
-        state
-    })
+  const newData = new codestate({
+    codeV: codeVerifier,
+    State: state,
+  });
 
-    newData.save();
-    
-    res.redirect(url)
+  newData.save();
 
-    //save code verifier and state
-})
+  res.redirect(url);
+
+  //save code verifier and state
+});
 
 //callback
 app.get("/callback", (req, res) => {
-    
-})
+  const { state, code } = req.query;
+  codestate.findOne({ State: state }, (err, result) => {
+    if (!err) {
+      if (!result) {
+        return res.status(400).send("stored tokens to not match!");
+      }
+
+      // client: loggedClient,
+      //     accessToken,
+      //     refreshToken,
+      const twitterData = twitterClient
+        .loginWithOAuth2({
+          code,
+          codeVerifier: result.codeV,
+          redirectUri: callbackUrl,
+        })
+        .then((x) => {
+          const toks = new tkn({
+            accessTkn: x.accessToken,
+            refreshTkn: x.refreshToken,
+          });
+
+          toks.save();
+          res.status(200).send("success!");
+        });
+    }
+  });
+});
 
 //tweet
 app.get("/tweet", (req, res) => {
+  
 
-})
+  tkn.find({}, (err, results) => {
+    const refreshTkn = results[0].refreshTkn;
+    console.log(refreshTkn);
+
+    // const {
+    //   client: refreshedClient,
+    //   accessToken,
+    //   refreshToken: newRefreshToken,
+    // } =
+
+    twitterClient.refreshOAuth2Token(refreshTkn).then(x => {
+      console.log("accesstoken", x.accessToken);
+      console.log("refreshtoken", x.refreshToken);
+
+      tkn.replaceOne(
+        { refreshTkn },
+        { accessTkn: x.accessToken, refreshTkn: x.refreshToken }
+      );
+        
+      x.client.v2.me().then(z => {
+        res.send(z.data)
+      });
+    });
+
+ 
+
+  });
+
+  
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("server ready");
-})
+  console.log("server ready");
+});
